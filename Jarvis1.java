e1713c1ccbceb1d40a13cab9a311a95f69c78e95
Saimanoranjan <123983511+Saimanoranjan@users.noreply.github.com>
2023-02-07 23:38:53 +0530

    practice on 7th Feb

---
+++ Jarvis1.java
@@ -0,0 +1,11 @@
+class Jarvis
+{
+
+ public static void main(String args[])
+
+ {
+
+  System.out.println("Hello Mr.Stark");
+}
+
+}
---
+++ Jarvis2.java
@@ -0,0 +1,29 @@
+class Jarvis
+{
+  public static void main(String args[])
+   
+  {
+    System.out.println("Hello Mr.Stark");
+    
+    first fs=new first();
+     
+    Display();
+
+    fs.greetings();
+    }
+
+  public static void Display()
+  
+{
+
+   System.out.println("Hello Ms.Pots");
+
+}
+  public void greetings()
+ 
+{
+
+  System.out.println("I am Jarvis I am here for your service");
+}
+
+}
